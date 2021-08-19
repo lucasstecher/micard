259d17e8cca71512e59a068eaaf041677364ce4d
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2021-08-19 16:04:16 -0300

    adding more text, containers and childs

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.cyan,
+                backgroundImage: AssetImage('images/picture.jpg'),
+              ),
+              Text(
+                'Lucas Stecher',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 25,
+                  letterSpacing: 2.5,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: [
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
